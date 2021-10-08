37a0b714af7f45f39ca65224fbabc07506e26192
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-10-08 14:45:49 +0400

    feat:ending explore.js

--- museum/js/explore.js
+++ museum/js/explore.js
@@ -1,67 +1,60 @@
-const spliter = document.querySelector('.spliter');
-const cardCur = document.querySelector('.card_cur');
-const img = document.querySelector('.card_cur img');
-spliter.onmousedown = splitStart;
-spliter.addEventListener('touchstart', splitStart);
-spliter.addEventListener('touchend', splitEnd);
-spliter.addEventListener('touchmove', splitAction);
-let moveInitial = 0;
-let moveX1 = 0;
-let moveX2 = 0;
-let w = img.offsetWidth;
-let imageInt = setInterval(() => {
-    if (img.complete){
-        w = img.offsetWidth;
-        clearInterval(imageInt);
-    }
+function explore(){
+    const spliter = document.querySelector('.spliter');
+    const cardCur = document.querySelector('.card_cur');
+    const img = document.querySelector('.card_cur img');
     
-}, 50);
+    let w = img.offsetWidth;
+    let pos;
 
-
-function splitStart (e) {
-    // e = e || window.event;
-    e.preventDefault();
-    moveInitial = spliter.offsetLeft;
-
-    if (e.type == 'touchstart') {
-        moveX1 = e.touches[0].clientX;
-    } else {
-        moveX1 = e.clientX;
-        document.onmouseup = splitEnd;
-        document.onmousemove = splitAction;
+    spliter.addEventListener("mousedown", slideReady);
+    /*and another function when the mouse button is released:*/
+    window.addEventListener("mouseup", slideFinish);
+    /*or touched (for touch screens:*/
+    spliter.addEventListener("touchstart", slideReady);
+     /*and released (for touch screens:*/
+    window.addEventListener("touchstop", slideFinish);
+    function slideReady(e) {
+      /*prevent any other actions that may occur when moving over the image:*/
+      e.preventDefault();
+      /*the slider is now clicked and ready to move:*/
+      clicked = 1;
+      /*execute a function when the slider is moved:*/
+      window.addEventListener("mousemove", slideMove);
+      window.addEventListener("touchmove", slideMove);
+    }
+    function slideFinish() {
+      /*the slider is no longer clicked:*/
+      clicked = 0;
+    }
+    function slideMove(e) {
+      
+      /*if the slider is no longer clicked, exit this function:*/
+      if (clicked == 0) return false;
+      /*get the cursor's x position:*/
+      pos = getCursorPos(e)
+      /*prevent the slider from being positioned outside the image:*/
+      if (pos < 0) pos = 0;
+      if (pos > w) pos = w;
+      /*execute a function that will resize the overlay image according to the cursor:*/
+      slide(pos);
+    }
+    function getCursorPos(e) {
+      let a, x = 0;
+      e = e || window.event;
+      /*get the x positions of the image:*/
+      a = img.getBoundingClientRect();
+      /*calculate the cursor's x coordinate, relative to the image:*/
+      x = e.pageX - a.left;
+      /*consider any page scrolling:*/
+      x = x - window.pageXOffset;
+      return x;
+    }
+    function slide(x) {
+      
+      cardCur.style.width = x + "px";
+      
+      spliter.style.left = cardCur.offsetWidth - (spliter.offsetWidth / 2) + "px";
     }
 }
 
-function splitAction (e) {
-    // e = e || window.event;
-
-    if (e.type == 'touchmove') {
-        moveX2 = moveX1 - e.touches[0].clientX;
-        moveX1 = e.touches[0].clientX;
-        
-    } else {
-        moveX2 = moveX1 - e.clientX;
-        moveX1 = e.clientX;
-        if (moveX2 > w){
-            moveX2 = w;
-        }
-    }
-    let wImg = spliter.offsetLeft - moveX2;
-    if (wImg > w){
-        wImg = w
-    }
-    if (wImg < 0){
-        wImg = 0
-    }
-    spliter.style.left = wImg + "px";
-    console.log(spliter.style.left);
-    cardCur.style.width = spliter.style.left;
-
-}
-  
-function splitEnd (e) {
-    
-
-    document.onmouseup = null;
-    document.onmousemove = null;
-}
+explore();
