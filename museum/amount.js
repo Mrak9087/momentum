17519dfa3430d825ca0b8429f79daeb1a72dccaa
Mrak9087 <59642198+Mrak9087@users.noreply.github.com>
2021-10-11 00:54:06 +0400

    feat:add time chenge

--- museum/amount.js
+++ museum/amount.js
@@ -1,40 +1,40 @@
-const btnBuy = document.querySelector('.btn_buy');
-const buy = document.querySelector('.buy');
-const btnClose = document.querySelector('.btn_close')
-const btnPay = document.querySelector('.pay_btn')
+// const btnBuy = document.querySelector('.btn_buy');
+// const buy = document.querySelector('.buy');
+// const btnClose = document.querySelector('.btn_close')
+// const btnPay = document.querySelector('.pay_btn')
 
 
-btnBuy.addEventListener('click',(e) => {
-    buy.classList.add('buy_show');
-})
+// btnBuy.addEventListener('click',(e) => {
+//     buy.classList.add('buy_show');
+// })
 
-btnClose.addEventListener('click',(e) => {
-    buy.classList.remove('buy_show');
-})
+// btnClose.addEventListener('click',(e) => {
+//     buy.classList.remove('buy_show');
+// })
 
-buy.addEventListener('click',(e) => {
-    if (e.target === buy){
-        buy.classList.remove('buy_show');
-    }
+// buy.addEventListener('click',(e) => {
+//     if (e.target === buy){
+//         buy.classList.remove('buy_show');
+//     }
     
-})
+// })
 
 
-btnPay.addEventListener('click', function (e) {
-    e.preventDefault();
-    const x = e.clientX;
-    const y = e.clientY;
-    const rect = btnPay.getBoundingClientRect();
+// btnPay.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const x = e.clientX;
+//     const y = e.clientY;
+//     const rect = btnPay.getBoundingClientRect();
 
-    const xInside =  e.clientX - rect.x; 
-    const yInside = e.clientY - rect.y;
+//     const xInside =  e.clientX - rect.x; 
+//     const yInside = e.clientY - rect.y;
 
-    const circle = document.createElement('span');
-    circle.classList.add('circle');
-    circle.style.top = yInside + 'px';
-    circle.style.left = xInside + 'px';
+//     const circle = document.createElement('span');
+//     circle.classList.add('circle');
+//     circle.style.top = yInside + 'px';
+//     circle.style.left = xInside + 'px';
 
-    this.appendChild(circle);
+//     this.appendChild(circle);
 
-    setTimeout(() => circle.remove(), 500);
-})
+//     setTimeout(() => circle.remove(), 500);
+// })
